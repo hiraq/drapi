cbf9dc0952bd4e48b68aa3cd6202aa4ab908c279
Hiraq <hrxwan@gmail.com>
2013-07-21 11:50:46 +0700

    unit test > request

--- src/Drapi/Request.php
+++ src/Drapi/Request.php
@@ -8,65 +8,123 @@ use Zend\Uri\Uri as Uri;
 
 class Request extends Object
 {		
-	private $method;
-	private $ip;	
+	private $method;	
 	private $params = array();
 	private $requestedHost;
 	private $requestedPath;	
 	private $requestedScheme = 'http://';
-	private $requestedUrl;
+	private $requestedUri;	
+	private $isHttpsOn = false;
 	protected $currentRequest = null;
-	protected $currentUri = null;
-	protected $authCallback = null;
+	protected $currentUri = null;	
 
+	/**
+	 * Construct
+	 */
 	public function __construct()
 	{
-		$this->currentRequest = new PhpRequest();
-		$this->currentUri = new Uri();
+		$this->currentRequest = new PhpRequest();	
+		$this->currentUri = new Uri();	
 	}
 
+	/**
+	 * Listen to current http request
+	 *
+	 * @access public
+	 * @return void
+	 */
 	public function listen()
 	{
 
-	}
+		/*
+		setup host & path
+		 */
+		$this->requestedHost = $this->currentRequest->getServer('HTTP_HOST');
+		$this->requestedPath = $this->currentRequest->getServer('REQUEST_URI');
 
-	public function isRequestValid()
-	{
-		
-	}
+		/*
+		setup https/http scheme
+		 */
+		$https = $this->currentRequest->getServer('HTTPS');
+		$this->isHttpsOn = !empty($https) && $https == 'on' ? true : false;
+		$this->requestedScheme = !$this->isHttpsOn ? 'http://' : 'https://';
 
-	public function setAuthCallback($callback)
-	{
+		//build uri to managed with Zend\Uri
+		$this->buildUri();
+	}	
 
-	}
-
+	/**
+	 * Get http method (GET,POST,PUT,DELETE)
+	 *
+	 * @access public
+	 * @return string
+	 */
 	public function getMethod()
 	{				
 		return $this->currentRequest->getMethod();
-	}
-
-	public function getIp()
-	{
-
-	}
-
-	public function getUri()
-	{
-
-	}
-
-	public function getUriPath()
-	{
-
-	}
-
-	public function getParams()
-	{
-
 	}	
 
-	private function buildUri()
+	/**
+	 * Get Uri object
+	 *
+	 * @access public
+	 * @return Uri
+	 */
+	public function getObjectUri()
 	{
+		return $this->currentUri;
+	}
 
+	/**
+	 * Get PhpRequest object
+	 *
+	 * @access public
+	 * @return PhpRequest
+	 */
+	public function getObjectRequest()
+	{
+		return $this->currentRequest;
+	}
+
+	/**
+	 * Get current uri path
+	 *
+	 * @access public
+	 * @return boolean|string
+	 */
+	public function getUriPath()
+	{
+		if (method_exists($this->requestedUri,'getPath')) {
+			return $this->requestedUri->getPath();
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get current request query parameters
+	 *
+	 * @access public
+	 * @return boolean|string
+	 */
+	public function getParams()
+	{
+		if (method_exists($this->requestedUri,'getQueryAsArray')) {
+			return $this->requestedUri->getQueryAsArray();
+		}
+
+		return false;
+	}	
+
+	/**
+	 * Build uri string
+	 *
+	 * @access private
+	 * @return void
+	 */
+	private function buildUri()
+	{		
+		$url = $this->requestedScheme.$this->requestedHost.$this->requestedPath;			
+		$this->requestedUri = $this->currentUri->parse($url);
 	}
 }
--- tests/Drapi/RequestTest.php
+++ tests/Drapi/RequestTest.php
@@ -47,6 +47,9 @@ class RequestTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue(is_a($this->__Request,'\Drapi\Request'));				
 	}
 
+	/**
+	 * Test get http request method	 
+	 */
 	public function testGetMethod()
 	{		
 		$this->__PhpRequest->expects($this->once())
@@ -54,10 +57,112 @@ class RequestTest extends PHPUnit_Framework_TestCase
 							->will($this->returnValue('GET'));
 
 		$this->mockingRequest();
-
-		$this->assertTrue(method_exists($this->__Request, 'getMethod'));
+		
 		$method = $this->__Request->getMethod();
 		$this->assertEquals('GET',$method);
+	}
+
+	/**
+	 * Test get object uri & request	 
+	 */
+	public function testGetObject()
+	{
+		$this->mockingRequest();
+		$uri = $this->__Request->getObjectUri();
+		$request = $this->__Request->getObjectRequest();
+
+		$this->assertEquals($uri,$this->__Uri);
+		$this->assertEquals($request,$this->__PhpRequest);
+	}
+
+	/**
+	 * Test listen current request	 
+	 */
+	public function testListen()
+	{		
+		/*
+		setup stubbed php request
+		 */
+		$this->__PhpRequest->expects($this->any())
+							->method('getServer')
+							->will($this->returnCallback(function($arg) {
+								switch($arg) {						   			
+						   			case 'HTTP_HOST':
+						   				return 'api.site.com';
+						   			break;
+
+						   			case 'REQUEST_URI':
+						   				return '/test/users?name=testing&key1=value1';
+						   			break;
+
+						   			case 'HTTPS':
+						   				return '';
+						   			break;
+
+						   			default:
+						   				return false;
+						   			break;
+						   		}
+							}));
+
+		/*
+		test stubbed uri object
+		 */
+		$this->__Uri->expects($this->any())
+					->method('parse')
+					->with($this->anything())
+					->will($this->returnSelf());
+
+		$this->__Uri->expects($this->any())
+					->method('getPath')					
+					->will($this->returnValue('/test/users?name=testing&key1=value1'));
+
+		$this->__Uri->expects($this->any())
+					->method('getQueryAsArray')					
+					->will($this->returnValue(array(
+						'name' => 'testing',
+						'key1' => 'value1'
+					)));
+
+		//create mocking objects
+	   	$this->mockingRequest();
+
+	   	/*
+	   	test stubbed objects
+	    */
+	   	$this->assertEquals('api.site.com',$this->__PhpRequest->getServer('HTTP_HOST'));
+	   	$this->assertEquals('',$this->__PhpRequest->getServer('HTTPS'));
+	   	$this->assertEquals('/test/users?name=testing&key1=value1',$this->__PhpRequest->getServer('REQUEST_URI'));
+	   	$this->assertEquals($this->__Uri,$this->__Uri->parse('test'));
+
+	   	//test listen query
+	   	$this->__Request->listen();
+	   	$path = $this->__Request->getUriPath();
+	   	$params = $this->__Request->getParams();
+
+	   	$this->assertEquals('/test/users?name=testing&key1=value1',$path);
+	   	$this->assertInternalType('array',$params);
+	}
+
+	/**
+	 * Test return false uri object	 
+	 */
+	public function testReturnFalseUri()
+	{
+		/*
+		fake method
+		 */
+		$this->__Uri->expects($this->any())
+					->method('getPath2')					
+					->will($this->returnValue('/test/users?name=testing&key1=value1'));
+
+		$this->mockingRequest();
+
+		$path = $this->__Request->getUriPath();
+		$params = $this->__Request->getParams();
+
+		$this->assertFalse($path);
+		$this->assertFalse($params);
 	}
 
 	/**
